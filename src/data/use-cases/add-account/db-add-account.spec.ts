8d0ccf2233ea79671e94d5e55eca1dc46c727e81
Alexandre Junior <alejunior.dev@gmail.com>
2024-03-19 11:32:01 -0300

    test: ensure db add account throws if add account repository throws

--- src/data/use-cases/add-account/db-add-account.spec.ts
+++ src/data/use-cases/add-account/db-add-account.spec.ts
@@ -103,4 +103,20 @@ describe('DbAddAccount UseCase', () => {
       password: 'hashed_password'
     })
   })
+
+  it('should throw if AddAccountRepository throws', async () => {
+    const { sut, addAccountRepositoryStub } = makeSut()
+    jest.spyOn(addAccountRepositoryStub, 'add').mockReturnValueOnce(new Promise((resolve, reject) => {
+      reject(new Error())
+    }))
+
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+
+    const promiseAccount = sut.add(accountData)
+    await expect(promiseAccount).rejects.toThrow()
+  })
 })
