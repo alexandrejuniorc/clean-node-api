b51873be367c0ee762fe561b8b6faeca7c251920
Alexandre Junior <alejunior.dev@gmail.com>
2024-03-28 18:12:01 -0300

    feat(server): ensure requests parse body as json

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,11 +1,6 @@
-import express from 'express'
+import { app } from './config/app'
 
-const app = express()
 const port = 3000
-
-app.get('/', (req, res) => {
-  return res.json({ message: 'Hello World' })
-})
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`)
