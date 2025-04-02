9ab52571bea39fa7e52e6a8b2bc398309fc3fe65
Alexandre Junior <alejunior.dev@gmail.com>
2025-04-01 21:38:48 -0300

    chore: remove project

--- src/main/config/app.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express'
-import setupMiddlewares from './middlewares'
-import setupRoutes from './routes'
-
-export const app = express()
-setupMiddlewares(app)
-setupRoutes(app)
--- src/main/server.ts
+++
@@ -1,7 +0,0 @@
-import { app } from './config/app'
-
-const port = 3000
-
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`)
-})
